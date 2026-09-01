cfc4f38268ec20179a9fd2da596030e5a8ddf517
uteau <guerrauteaub@gmail.com>
2026-09-01 17:21:04 -0400

    fix(env): use publishable key naming from supabase

--- lib/supabase/browser.ts
+++ lib/supabase/browser.ts
@@ -3,6 +3,6 @@ import { createBrowserClient as createBrowserClientSsr } from '@supabase/ssr'
 export function createBrowserClient() {
   return createBrowserClientSsr(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!
   )
 }
